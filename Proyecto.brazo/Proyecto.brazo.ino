6f4590b33fe9af9e2154e26698be46ebb8c5af00
GOESTH <adiaztorres590@gmail.com>
2018-05-01 12:45:02 -0500

    proyecto.brazo

--- Proyecto.brazo/Proyecto.brazo.ino
+++ Proyecto.brazo/Proyecto.brazo.ino
@@ -1,15 +1,17 @@
 
 #include <Servo.h>
-Servo adat; //adelante y atras
-Servo arab;//arriba y abajo
-Servo gar;//garra
+Servo adat; //adelante y atras.
+Servo arab;//arriba y abajo.
+Servo gar;//garra.
 
 
 void setup() {
 
-adat.attach(9);
-arab.attach(10);
-gar.attach(11);
+adat.attach(9);//el servo adat se ubica en el pin digital(pwm)9.
+arab.attach(10);//el servo arab se ubica en el pin digital(pwm)10.
+gar.attach(11);//el servo gar se ubica en el pin digital(pwm)11.
+
+
 
 }
 
