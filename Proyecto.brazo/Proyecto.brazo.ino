ef4b5d50fbb16e959e3d15fe5d78412c4132347d
GOESTH <adiaztorres590@gmail.com>
2018-05-01 12:57:28 -0500

    void setup completo V0.1

--- Proyecto.brazo/Proyecto.brazo.ino
+++ Proyecto.brazo/Proyecto.brazo.ino
@@ -6,16 +6,22 @@ Servo gar;//garra.
 
 
 void setup() {
-
+  
+Serial.begin(9600);
 adat.attach(9);//el servo adat se ubica en el pin digital(pwm)9.
 arab.attach(10);//el servo arab se ubica en el pin digital(pwm)10.
 gar.attach(11);//el servo gar se ubica en el pin digital(pwm)11.
 
-
+char cmd;//lee un caracter, que es el comando para saber en que poscicion se ubica el brazo
 
 }
 
 
 void loop() {
+
+  //El brazo de mueve en dos ejes X y Y
+
+
+  
 
 }
