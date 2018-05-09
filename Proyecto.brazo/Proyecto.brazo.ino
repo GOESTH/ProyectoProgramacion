fb37c5f44a06be1bf6d3b4629c9a45c63d0f092d
GOESTH <adiaztorres590@gmail.com>
2018-05-09 00:48:43 -0500

    reconocimiento de color agregado, creacion de una estructura switc para mover el brazo a una posicion segun el color, interfaz de usuario desabilitada, V 1.4

--- Proyecto.brazo/Proyecto.brazo.ino
+++ Proyecto.brazo/Proyecto.brazo.ino
@@ -1,23 +1,36 @@
 
+
+
 #include <Servo.h>
+
 Servo adat; //adelante y atras.
 Servo arab;//arriba y abajo.
 Servo gar;//garra.
+
 int velmo = 5;// Tiempo (milisegundos)que tarda en dar un paso el motor paso a paso.
 int anges = 0;// Angulo escogido por el caracter seleccionado.
 int posicion_motor = 0;// grado en el que esta el motor paso a paso
-
+int frequencia = 0;
+int numColor = 0;
 
 void setup() {
   
 Serial.begin(9600);
 
-pinMode(12, INPUT);
-pinMode(2, OUTPUT);
-pinMode(11, OUTPUT); // Pin 11 se conecta a al pin IN4 de la placa del motor paso a paso.
-pinMode(10, OUTPUT); // Pin 10 se conecta a al pin IN3 de la placa del motor paso a paso.
-pinMode(9, OUTPUT); // Pin 9 se conecta a al pin IN2 de la placa del motor paso a paso.
-pinMode(8, OUTPUT); // Pin 8 se conecta a al pin IN1 de la placa del motor paso a paso.
+  pinMode(4, OUTPUT);//pin S0 conectado al pin 4 del arduino
+  pinMode(7, OUTPUT);//pin S1 conectado al pin 7 del arduino
+  pinMode(2, OUTPUT);//pin S2 conectado al pin 2 del arduino
+  pinMode(12, OUTPUT);//pin S3 conectado al pin 12 del arduino
+  pinMode(13, INPUT);//pin S4 conectado al pin 13 del arduino
+
+  //se crea un estado base, poniendo el pín 4 HIGH y el pin 7 LOW para leer el los milisegundos que toma en cambiar e inicar la lectura de las frecuecnias por colores.
+  digitalWrite(4, HIGH);
+  digitalWrite(7, LOW);
+
+  pinMode(11, OUTPUT); // Pin 11 se conecta a al pin IN4 de la placa del motor paso a paso.
+  pinMode(10, OUTPUT); // Pin 10 se conecta a al pin IN3 de la placa del motor paso a paso.
+  pinMode(9, OUTPUT); // Pin 9 se conecta a al pin IN2 de la placa del motor paso a paso.
+  pinMode(8, OUTPUT); // Pin 8 se conecta a al pin IN1 de la placa del motor paso a paso.
 
 digitalWrite(2, LOW); 
 
@@ -29,6 +42,7 @@ adat.write(24);
 arab.write(85);
 gar.write(110);
 
+/*
 Serial.println("    -SERVO 1, MUEVE LA GARRA ADELANTE Y ATRAS");
 Serial.println("    -SERVO 2, MUEVE LA GARRA ARRIBA Y ABAJO");
 Serial.println("    -SERVO 2, ABRE Y CIERRA LA GARRA");
@@ -41,13 +55,144 @@ Serial.println("  POSICION c, SERVO 1: 70°, SERVO 2: 110°, garra: abierta, ROT
 Serial.println("  POSICION d, SERVO 1: 90°, SERVO 2: 77°, garra: cerrar, ROTACION: 270°");
 Serial.println("  POSICION e, EL BRAZO SE MUEVE SEGUN EL JOYSTICK");
 Serial.println("  POR DEFECTO SE INICIA EN LA POSICION, SERVO 1: 24°, SERVO 2: 85°, garra: cerrada, ROTACION: 0° !!! ");
-
+*/
 
 }
 
 void loop() {
-
   
+numColor = readColor();
+  delay(10);  
+  switch (numColor) {
+    case 1:
+    
+    adat.write(27);//se escribe el numero de grados que se va a mover el servo 1.
+    arab.write(77);//se escribe el numero de grados que se va a mover el servo 2.
+    gar.write(145);//se escribe el numero de grados que se va a mover el servo 3.
+    
+    anges =0;
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+
+    adat.write(50);
+    arab.write(90);
+    gar.write(120);
+    
+    break;
+    case 2:
+    
+    adat.write(27);//se escribe el numero de grados que se va a mover el servo 1.
+    arab.write(77);//se escribe el numero de grados que se va a mover el servo 2.
+    gar.write(145);//se escribe el numero de grados que se va a mover el servo 3.
+
+    
+    anges =90;
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+
+    adat.write(50);
+    arab.write(90);
+    gar.write(120);
+    
+    break;
+    case 3:
+    
+    adat.write(27);//se escribe el numero de grados que se va a mover el servo 1.
+    arab.write(77);//se escribe el numero de grados que se va a mover el servo 2.
+    gar.write(145);//se escribe el numero de grados que se va a mover el servo 3.
+
+    
+    anges =180;
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+
+    adat.write(50);
+    arab.write(90);
+    gar.write(120);
+    
+    break;
+    case 4:
+  
+    adat.write(27);//se escribe el numero de grados que se va a mover el servo 1.
+    arab.write(77);//se escribe el numero de grados que se va a mover el servo 2.
+    gar.write(145);//se escribe el numero de grados que se va a mover el servo 3.
+
+    
+    anges =270;
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+
+    adat.write(50);
+    arab.write(90);
+    gar.write(120);
+    
+    break;
+    case 5:
+    adat.write(27);//se escribe el numero de grados que se va a mover el servo 1.
+    arab.write(77);//se escribe el numero de grados que se va a mover el servo 2.
+    gar.write(145);//se escribe el numero de grados que se va a mover el servo 3.
+
+    
+    anges =360;
+    anges = (anges * 1.4222222222);
+
+  while (anges>posicion_motor){   // Giro hacia la izquierda en grados
+       paso_izq();
+       posicion_motor = posicion_motor + 1;
+   }
+   
+  while (anges<posicion_motor){   // Giro hacia la derecha en grados
+        paso_der();
+        posicion_motor = posicion_motor -1;
+   }
+
+    adat.write(50);
+    arab.write(90);
+    gar.write(120);
+    
+    break;
+    
+    case 0:
+    break;
+  }
+
+  /*
 if(Serial.available()>=1)
    {
     
@@ -157,22 +302,24 @@ if(Serial.available()>=1)
     pot1 = map(pot1, 0, 1023, 0, 180);// Se mapea el valor del potenciometro en una escala de 0 a 180 grados, rango de giro de cada servo motor.
     int pot2 = analogRead(1);// Lee el valor del potenciometro 1 del joystick, este valor entra por el pin analogico 1 (valor desde 0 a 1023)
     pot2 = map(pot2, 0, 1023, 0, 180);// Se mapea el valor del potenciometro en una escala de 0 a 180 grados, rango de giro de cada servo motor.
-    int pot3  = analogRead(2);
-    int botgar = digitalRead(12);
-    int pulsador = digitalRead(13);//lee el valor binario del pin digital 13, para asi salir del bucle cambiando el caracter de la variable cmd.
+ 
+    int botonera = analogRead(2);//lee el valor de la resistencia que genera un boton u otro que estan en el mismo pin analogo, para asi salir del bucle cambiando el caracter de la variable cmd y cerrar y abrir la garra.
     
   adat.write(pot1);//se manda la orden al servo motor 1 para que se mueva segun el joystick.
   arab.write(pot2);//se manda la orden al servo motor 2 para que se mueva segun el joystick.
   
-  if (botgar == HIGH){  
+  if (botonera < x && botonera > x){  
 
     gar.write(110);//se manda la orden al servo motor 3 para que se mueva segun un boton.
   
-  }else if (botgar == LOW){
+  }else if (botonera < x && botonera > x){
 
   gar.write(140);//se manda la orden al servo motor 3 para que se mueva segun un boton.  
  
-  }
+  }else if (botonera < x && botonera > x){
+
+     cmd = 'a';
+    }
   
     anges = pot3;
     anges = (anges * 1.4222222222);
@@ -195,6 +342,7 @@ if(Serial.available()>=1)
    }
     }
      }
+     */
       }
   
 
@@ -251,4 +399,72 @@ void apagado() {// funcion_apagado del motor paso a paso.
  digitalWrite(8, LOW);  
  }
 
+
+  int reconocerColor() {
+    /*
+     * Para reconocer que combinacion de RGB (red, green, blue) o RVA(rojo, verde, azul), 
+     * se encienden y se apagan los pines 4 y 5 a los que estan conectados S2 y S3 del sensor de color, 
+     * para asi medir en milisegundos la frecuencia de cada color y ver segun esa combinacion que color es.
+     */
+    
+  digitalWrite(4, LOW);
+  digitalWrite(5, LOW);
+ 
+  frecuencia = pulseIn(6, LOW); // se lee la frecuencia que son el valor de los micrasegundos en los que el puerto 6 pasa de ser HIGH a ser LOW, cada color tiene su propia frecuencia.
   
+  int R = frequencia;//El sensor lee RGB (Rojo, Verde, Azul), aca se busca la frecuencia del rojo, su intensidad en el color al que esta expuesto el sensor.
+
+
+  Serial.print("Rojo: ");
+  Serial.print(frequencia);
+  Serial.print("  ");
+  delay(50);
+  
+
+  digitalWrite(4, HIGH);
+  digitalWrite(5, HIGH);
+
+
+  frequency = pulseIn(6, LOW);// se lee la frecuencia que son el valor de los micrasegundos en los que el puerto 6 pasa de ser HIGH a ser LOW, cada color tiene su propia frecuencia.
+  
+  int V = frequencia;//El sensor lee RGB (Rojo, Verde, Azul), aca se busca la frecuencia del Rojo, su intensidad en el color al que esta expuesto el sensor
+
+ 
+  Serial.print("Verde: ");
+  Serial.print(frequencia);// se lee la frecuencia que son el m¿valor de los micrasegundos en los que el puerto 6 pasa de ser HIGH a ser LOW, cada color tiene su propia frecuencia.
+  Serial.print("  ");
+  delay(50);
+
+
+  digitalWrite(4, LOW);
+  digitalWrite(5, HIGH);
+
+  frequencia = pulseIn(6, LOW);// se lee la frecuencia que son el valor de los micrasegundos en los que el puerto 6 pasa de ser HIGH a ser LOW, cada color tiene su propia frecuencia.
+  int A = frequencia;//El sensor lee RGB (Rojo, Verde, Azul), aca se busca la frecuencia del azul, su intensidad en el color al que esta expuesto el sensor.
+  
+  Serial.print("Azul: ");
+  Serial.print(frequencia);// se lee la frecuencia que son el m¿valor de los micrasegundos en los que el puerto 6 pasa de ser HIGH a ser LOW, cada color tiene su propia frecuencia.
+  Serial.println("  ");
+  delay(50);
+
+  
+  if(R<45 & R>32 & V<65 & V>55){// se comprueba que segun la combinacion de frecuencias del rojo y verde, el sensor este analizando un color rojo.
+    numColor = 1; 
+  }
+  if(V<55 & V>43 & A<47 &A>35){// se comprueba que segun la combinacion de frecuencias del verde y azul, el sensor este analizando un color naranja.
+    numColor = 2; 
+  }
+  if(R<53 & R>40 & V<53 & V>40){// se comprueba que segun la combinacion de frecuencias del rojo y verde, el sensor este analizando un color verde.
+    numColor = 3; 
+  }
+  if(R<38 & R>24 & V<44 & V>30){// se comprueba que segun la combinacion de frecuencias del rojo y verde, el sensor este analizando un color amrillo.
+    numColor = 4;
+  }
+  if(R<56 & R>46 & V<65 & V>55){// se comprueba que segun la combinacion de frecuencias del rojo y verde, el sensor este analizando un color cafe.
+    numColor = 5; 
+  }
+  if (V<58 & V>45 & A<40 &A>26){// se comprueba que segun la combinacion de frecuencias del verde y azul, el sensor este analizando un color azul.
+    numColor = 6; 
+  }
+  return numColor;  //devuelve el numero que tiene la variable numColor para asi ser evaluado en el switch.
+}
